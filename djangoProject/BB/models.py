4a735b69fdd0910ec7ee3e11a81fa8b145a66c10
Leonid2004 <leophilo7@gmail.com>
2021-07-13 23:45:13 +0200

    StrangeProblem with pictures

--- djangoProject/BB/models.py
+++ djangoProject/BB/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
     text = models.CharField(max_length=3500)
-    whoLiked = models.OneToOneField(User,on_delete=models.CASCADE,)
+    whoLiked = models.ForeignKey(User,on_delete=models.CASCADE,)
     Likes = models.IntegerField(default=0)
     Dislikes = models.IntegerField(default=0)
+    filesForWeb = models.ImageField(upload_to='images/',default="")
 
 class Messages(models.Model):
     text = models.CharField(max_length=1500)
